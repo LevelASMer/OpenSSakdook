a12fe045ac333cca164dacb5795303a434ccec89
LevelASMer <zx132435@naver.com>
2019-09-14 23:14:40 +0900

    Added Filter

--- views.py
+++ views.py
@@ -24,6 +24,12 @@ class CommandSchema(colander.MappingSchema):
     description = colander.SchemaNode(colander.String(),  widget=TextAreaWidget())
     cooltime = colander.SchemaNode(colander.String(), widget=TextAreaWidget())
 
+class WordSchema(colander.MappingSchema):
+    word = colander.SchemaNode(colander.String(), widget=TextAreaWidget())
+
+class URLSchema(colander.MappingSchema):
+    url = colander.SchemaNode(colander.String(), widget=TextAreaWidget())
+
 class DeleteSchema(colander.MappingSchema):
     pass
 
@@ -341,4 +347,259 @@ def timer_delete(request):
 
 @view_config(route_name='filter', renderer='template/filters.pt')
 def filter(request):
-    return {"layout": site_layout()}
+    con = sqlite3.connect(DATABASE)
+    c = con.cursor()
+
+    c.execute('SELECT id, name FROM filters_word ORDER BY name DESC')
+    word = c.fetchall()
+    c.execute('SELECT id, url FROM filters_link ORDER BY url DESC')
+    url = c.fetchall()
+    con.close()
+
+    return {"layout": site_layout(), 'word': word, 'url': url, }
+
+@view_config(route_name='word_add', renderer='template/add_word.pt')
+def word_add(request):
+    schema = WordSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+
+                # Save form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (appstruct["word"], )
+                c.execute('INSERT OR IGNORE INTO filters_word (name) VALUES(?)', values)
+                con.commit()
+                con.close()
+
+                # Thank user and take him/her to the next page
+                request.session.flash('Thank you for the submission.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, }
+
+@view_config(route_name='word_edit', renderer='template/edit_word.pt')
+def word_edit(request):
+    id = request.matchdict['id']
+    schema = WordSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+
+                # Save form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (appstruct["word"], id, )
+                c.execute('UPDATE filters_word SET name=? WHERE id=?', values)
+                con.commit()
+                con.close()
+
+                # Thank user and take him/her to the next page
+                request.session.flash('Thank you for the submission.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    # Get data from database
+    con = sqlite3.connect(DATABASE)
+    c = con.cursor()
+
+    values = (id, )
+    c.execute('SELECT name FROM filters_word WHERE id=?', values)
+    fetch = c.fetchall()
+    con.close()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, "id": id, "word": fetch[0], }
+
+@view_config(route_name='word_delete', renderer='template/delete_word.pt')
+def word_delete(request):
+    id = request.matchdict['id']
+    schema = DeleteSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+                # Delete form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (id, )
+                c.execute('DELETE FROM filters_word WHERE id=?', values)
+                con.commit()
+                con.close()
+
+                request.session.flash('Goodbye.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, "id": id, }
+
+@view_config(route_name='url_add', renderer='template/add_url.pt')
+def url_add(request):
+    schema = URLSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+
+                # Save form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (appstruct["url"], )
+                c.execute('INSERT OR IGNORE INTO filters_link (url) VALUES(?)', values)
+                con.commit()
+                con.close()
+
+                # Thank user and take him/her to the next page
+                request.session.flash('Thank you for the submission.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, }
+
+@view_config(route_name='url_edit', renderer='template/edit_url.pt')
+def url_edit(request):
+    id = request.matchdict['id']
+    schema = URLSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+
+                # Save form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (appstruct["url"], id, )
+                c.execute('UPDATE filters_url SET name=? WHERE id=?', values)
+                con.commit()
+                con.close()
+
+                # Thank user and take him/her to the next page
+                request.session.flash('Thank you for the submission.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    # Get data from database
+    con = sqlite3.connect(DATABASE)
+    c = con.cursor()
+
+    values = (id, )
+    c.execute('SELECT url FROM filters_link WHERE id=?', values)
+    fetch = c.fetchall()
+    con.close()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, "id": id, "url": fetch[0], }
+
+@view_config(route_name='url_delete', renderer='template/delete_url.pt')
+def url_delete(request):
+    id = request.matchdict['id']
+    schema = DeleteSchema().bind(request=request)
+
+    # Create a styled button with some extra Bootstrap 3 CSS classes
+    process_btn = deform.form.Button(name='process')
+    form = deform.form.Form(schema, buttons=(process_btn,))
+
+    # User submitted this form
+    if request.method == "POST":
+        if 'process' in request.POST:
+            try:
+                appstruct = form.validate(request.POST.items())
+                # Delete form data from appstruct
+                con = sqlite3.connect(DATABASE)
+                c = con.cursor()
+
+                values = (id, )
+                c.execute('DELETE FROM filters_link WHERE id=?', values)
+                con.commit()
+                con.close()
+
+                request.session.flash('Goodbye.')
+
+                # Redirect to the page shows after succesful form submission
+                return HTTPFound("/filter/list/1")
+            except deform.exception.ValidationFailure as e:
+                # Render a form version where errors are visible next to the fields,
+                # and the submitted values are posted back
+                rendered_form = e.render()
+    else:
+        # Render a form with initial default values
+        rendered_form = form.render()
+
+    return {"layout": site_layout(), "rendered_form": rendered_form, "id": id, }
